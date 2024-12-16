790d0f61d34be97e4a290d6ce6e3e6b761f6acf2
Hacker-Sam-is-here <jrdowney006@gmail.com>
2024-12-16 12:16:32 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Syntax Revision/maths_ops.cpp

---
+++ Syntax Revision/maths_ops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+    //Arithmetic Operators
+    // operations = +, -, *, /, %
+
+    //addition
+    int students = 20;
+    students = students + 2;
+    students += 2; // same as students = students + 2
+    students ++; // same as students = students + 1
+
+    //subtraction
+    students = students - 2;
+    students -= 2; // same as students = students - 2
+    students --; // same as students = students - 1
+
+
+    return 0;
+}
