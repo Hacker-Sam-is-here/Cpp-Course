452306853cce51150be9d39832829ac785ef665f
Hacker-Sam-is-here <jrdowney006@gmail.com>
2024-12-16 13:59:50 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Syntax Revision/Chapter9 - ifs.cpp
    	renamed:    Syntax Revision/Practice Program - 1.cpp -> Syntax Revision/Hypotenuse Program.cpp

--- Syntax Revision/Chapter9 - ifs.cpp
+++ Syntax Revision/Chapter9 - ifs.cpp
@@ -4,15 +4,37 @@
 #include <iostream>
 
 int main(){
+
+    /*
+    from python to c++
+    if = if
+    elif = else if
+    else = else
+    PS: sirf thoda syntax ka farak h baaki same hi h
+    */
+
     int age;
 
     std::cout << "Enter your age: ";
     std::cin >> age;
     
-    if(age >= 18){
+    if(age >= 18){ //agar age 18 se badi h
         std::cout << "You are an adult" << std::endl;
     }
+    else if(age >= 13){ // agar age 13 se badi h
+
+
+    //ye sawal hoga ki age 18 se bhi badi h aur 13 se to kya aayega
+    // 18 wali condition pehle exececute hojayegi to code run nahi karega uske further
+    // agar 13 wali condition upar hoti to dono chije print hoti
+        std::cout << "You are a teenager" << std::endl;
+    }
+
+    else if(age == 12){ // agar age 12 ha
+        std::cout << "Chal be Nibbe" << std::endl;
+    }
+
     else{
-        std::cout << "You are not an adult" << std::endl;
+        std::cout << "Ja Bacche POGO dekh" << std::endl;
     }
 }
